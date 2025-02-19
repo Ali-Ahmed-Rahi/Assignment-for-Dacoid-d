c3b98ab34f65dcf2b6db898163f6f732aa20c224
rahiji9 <aliahmedrahi007@gmail.com>
2025-02-19 20:34:11 +0600

    vercel releted problem

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,12 +1,11 @@
 
-import { Link } from "react-router-dom";
 
 const Scoreboard = ({ score, total }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md w-96 text-center">
       <h2 className="text-2xl font-bold">Quiz Completed!</h2>
       <p className="mt-2 text-lg">Your score: {score} / {total}</p>
-      <Link to="/">Back to Home</Link>
+     
     </div>
   );
 };
